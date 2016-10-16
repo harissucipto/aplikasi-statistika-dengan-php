9b467a7c2f5d1508b23ccb0418a3b78a6ced111c
Haris Sucipto <harisssucipto@gmail.com>
2016-10-16 13:40:43 +0700

    selesaikan interface input data

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
+<!DOCTYPE html>
+<html>
 
-    <h1>Aplikasi Statistikaa Sederhana</h1>
+<head>
+    <title>Aplikasi sederhana sederhana</title>
+</head>
+
+<body>
+    <h1>Aplikasi Statistika Sederhana</h1>
 
     <!-- banyak input datanya-->
     <form action="index.php" method="GET">
@@ -11,12 +18,29 @@
         </p>
     </form>
 
-    <?
+    <form action="proses.php" method="GET">
+
+    <? // mulai skrip php
     // ambil nilai yang telah dikirim 
     $inputData = $_GET[inputData];
     $jdata = $_GET[jdata];
 
     if ($inputData) {
-        echo $inputData;
-    }       
+        // lakukan pengulangan sebanyak data yang akan dimasukan
+        for ($i = 1; $i <= $jdata; $i++) { 
+            echo "
+                    <p>Masukan data$i:
+                        <input type='text' name='data$i'>
+                    </p>
+            "; // akhir echo
+        } // akhir pengulangan
+    } //akhir kondisi    
     ?>
+            <p>
+                <input type="submit" name="proses" value="proses">
+            </p>
+    </form>
+
+</body>
+
+</html>
